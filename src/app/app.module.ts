3211009c093c36eccccb9f4db9fff7286aaa7ce2
priyatosh <priyatoshpatra@gmail.com>
2018-05-31 18:51:15 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ValueComponent } from './value/value.component';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ValueComponent],
+  imports: [BrowserModule, HttpModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
